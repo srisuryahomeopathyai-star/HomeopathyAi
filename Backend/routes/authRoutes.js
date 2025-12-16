cf1dc6ed292574276db6a71c0f62c2df4711ec77
mounikavemulapalli <124512238+mounikavemulapalli@users.noreply.github.com>
2025-12-16 12:58:18 +0530

    modified port and pushing the changes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -145,12 +145,15 @@ router.post("/login", async (req, res) => {
       try {
         const transporter = nodemailer.createTransport({
           host: process.env.SMTP_HOST || "smtp.gmail.com",
-          port: Number(process.env.SMTP_PORT || 587),
-         secure: process.env.SMTP_PORT == 465,
-          auth: process.env.SMTP_USER && process.env.SMTP_PASS ? {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-          } : undefined,
+          port: Number(process.env.SMTP_PORT) || 465,
+          secure: Number(process.env.SMTP_PORT) === 465,
+          auth:
+            process.env.SMTP_USER && process.env.SMTP_PASS
+              ? {
+                  user: process.env.SMTP_USER,
+                  pass: process.env.SMTP_PASS,
+                }
+              : undefined,
         });
         await transporter.sendMail({
           from: process.env.SMTP_FROM || adminEmail,
